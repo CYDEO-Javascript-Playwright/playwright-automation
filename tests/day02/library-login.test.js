b1f3db388994da3059e9feca29d186a88448689b
Muhtar <Muhtar.Mahmut0519@gmail.com>
2024-07-20 11:38:36 -0400

    Playwright Day02

---
+++ tests/day02/library-login.test.js
@@ -0,0 +1,8 @@
+import { test, expect } from "@playwright/test";
+
+test("Libraray Login", async ({ page }) => {
+  // go to to https://library2.cydeo.com/
+  await page.goto("https://library2.cydeo.com/");
+
+  
+});
