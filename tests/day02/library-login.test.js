862e086c50913ecda6922dfaf4153a375eea411e
Muhtar <Muhtar.Mahmut0519@gmail.com>
2024-07-20 11:51:45 -0400

    Playwright Day02

--- tests/day02/library-login.test.js
+++ tests/day02/library-login.test.js
@@ -1,8 +1,31 @@
-import { test, expect } from "@playwright/test";
+import { test } from "@playwright/test";
 
 test("Libraray Login", async ({ page }) => {
   // go to to https://library2.cydeo.com/
   await page.goto("https://library2.cydeo.com/");
 
+  // create locator variable named usernameInput
+  const usernameInput = await page.locator("//input[@id='inputEmail']");
+
+  // create locator variable named passwordInput and use this xpath //input[@id='inputPassword'] to locate
+  const passwordInput = await page.locator("//input[@id='inputPassword']");
+
+  // create locator variable named signinButton and use this xpath //button[@type='submit'] to locate
+  const signinButton = await page.locator("//button[@type='submit']");
+
+  // pause the automation for 2 seconds
+  await page.waitForTimeout(2000);
+
+  // enter username and password
+  await usernameInput.fill("librarian10@library");
+
+  await page.waitForTimeout(2000);
+
+  await passwordInput.fill("libraryUser");
+
+  // click on signin button
+  await signinButton.click();
+
+  await page.waitForTimeout(2000);
   
 });
