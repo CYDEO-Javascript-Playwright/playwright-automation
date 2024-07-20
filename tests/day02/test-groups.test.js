8f041349615109a75f008ce7d915b7d4b7e91745
Muhtar <Muhtar.Mahmut0519@gmail.com>
2024-07-20 14:25:55 -0400

    Playwright Day02

--- tests/day02/test-groups.test.js
+++ tests/day02/test-groups.test.js
@@ -1,5 +1,45 @@
 import test from "playwright/test";
 
+
+test.describe("Cydeo practice website tests", async () => {
+
+    test.beforeEach( async ({page}) => { 
+        await page.goto("https://practice.cydeo.com/");
+    });
+
+    test.afterEach( async ({page}) => { 
+        await page.waitForTimeout(2000);
+    });
+
+
+  test("A/B Testing is clickable", async ({page}) => {
+    
+    const abTestingElement = await page.locator("text='A/B Testing'");
+    console.log(`A/B Testing is clickable: ${await abTestingElement.isEnabled()}`);
+    abTestingElement.click();
+
+  });
+
+  test("Add/Remove Elements is enabled", async ({page}) => {
+    const addRemoveElements = await page.locator("text='Add/Remove Elements'");
+    console.log(`Add/Remove Elements is enabled: ${await addRemoveElements.isEnabled()}`);
+
+  });
+
+  test("Autocomplete is displayed", async ({page}) => {
+    const autocomplete = await page.locator("text='Autocomplete'");
+    console.log(`Autocomplete is displayed: ${await autocomplete.isVisible()}`);
+
+  });
+
+});
+
+
+
+
+// ----------------------------------------------------------------
+
+
 test.describe("Test Group", async () => {
 
     test.beforeAll( async () => { 
@@ -32,3 +72,4 @@ test.describe("Test Group", async () => {
 
 
 });
+
