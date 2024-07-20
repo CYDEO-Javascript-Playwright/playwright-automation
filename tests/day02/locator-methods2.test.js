d1b9cbf2db29f49818445c7a0e136d562b466666
Muhtar <Muhtar.Mahmut0519@gmail.com>
2024-07-20 13:40:35 -0400

    Playwright Day02

--- tests/day02/locator-methods2.test.js
+++ tests/day02/locator-methods2.test.js
@@ -23,8 +23,40 @@ test("InnerText method test", async ({ page }) => {
 
 test('inputValue method test: only works with <input> or <textarea> or <select>', async ({ page }) => {
     
+    // go to https://practice.cydeo.com/inputs
+     
+    await page.goto('https://practice.cydeo.com/inputs');
+
+    // create locator variable named inputBox with the xpath //input[@type='number']
+     
+    const inputBox = await page.locator("//input[@type='number']");
+
+    await inputBox.fill("1200");
+
+    await page.waitForTimeout(3000);
+
+    const inputValue = await inputBox.inputValue();
+
+    console.log(inputValue);
 
 
 });
+
+
+test('getAttribute method test: can get the value of an attribute of the element', async ({ page }) => {
+    await page.goto('https://practice.cydeo.com/inputs');
+
+    const cydeo = await page.locator("text='CYDEO'");
+
+    const elementLink = await cydeo.getAttribute("href");
+
+    console.log(`Element's link:  ${elementLink}`);
+
+});
+
+
+/*
+<a target="_blank" href="https://cydeo.com/">CYDEO</a>
+*/
 
 
