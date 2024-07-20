d87daa02baa7ce1ab7c08922abd5c11a2987dd41
Muhtar <Muhtar.Mahmut0519@gmail.com>
2024-07-20 12:51:32 -0400

    Playwright day02

---
+++ tests/day02/locator-methods.test.js
@@ -0,0 +1,27 @@
+import { test } from "@playwright/test";
+
+test("check method test", async ({ page }) => {
+  // go to https://practice.cydeo.com/checkboxes
+  await page.goto("https://practice.cydeo.com/checkboxes");
+
+  // create locator variable named checkBox1 with this xpath //input[@id='box1']
+  const checkBox1 = await page.locator('//input[@id="box1"]');
+
+  await page.waitForTimeout(3000);
+
+  await checkBox1.check();
+
+  // pause the automation for 3 seconds
+  await page.waitForTimeout(3000);
+
+  // create locator variable named checkBox2 with this xpath //input[@id='box2']
+  const checkBox2 = await page.locator('//input[@id="box2"]');
+
+  await page.waitForTimeout(3000);
+
+  await checkBox2.uncheck();
+
+  await page.waitForTimeout(3000);
+
+  
+});
