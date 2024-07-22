8d1395814c3d833372a27987e74b4bcc4c038e5a
Muhtar <Muhtar.Mahmut0519@gmail.com>
2024-07-22 19:59:57 -0400

    Playwright Day03

--- tests/day03/assertion-practice1.test.js
+++ tests/day03/assertion-practice1.test.js
@@ -1,28 +1,37 @@
 import { test, expect } from "@playwright/test";
 
-test.describe("Assertion in UI testing", async () => {
+test.describe("Assertion Practice in UI testing", async () => {
 
   test.beforeEach(async ({ page }) => {
     await page.goto("https://practice.cydeo.com/");
   });
 
   test("Verify the page title is 'Practice'", async ({page}) => {
-
+        const pageTitle = await page.title();
+         expect(pageTitle).toEqual("Practice");
+       // await expect(page).toHaveTitle("Practice");
   });
 
   test("Verify the text 'Automation' is included the header element", async ({page}) => {
-
+    const headerElement =  page.locator('h1');
+    const headerText = await headerElement.textContent();
+    expect(headerText).toContain("Automation");
   });
 
   test("Verify the element 'A/B Testing' is clickable", async ({page}) => {
+    const abTestingLink = page.locator('a[href="/abtest"]');
 
+    expect(await abTestingLink.isEnabled()).toBeTruthy(); 
+    await expect(abTestingLink).toBeEnabled();
   });
 
   test("Verify the element 'Autocomplete' href is '/autocomplete' ", async ({page}) => {
-
+    const autocompleteLink = page.locator('a[href="/autocomplete"]');
+    const href = await autocompleteLink.getAttribute('href');
+    expect(href).toBe("/autocomplete");
+    await expect(autocompleteLink).toHaveAttribute("href", "/autocomplete");
   });
 
 });
-
 
 // come back at 7:45pm
