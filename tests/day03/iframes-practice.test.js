e26f084571afda9ca96034064a645d87b395a16b
Muhtar <Muhtar.Mahmut0519@gmail.com>
2024-07-22 20:55:45 -0400

    Playwright Day03

--- tests/day03/iframes-practice.test.js
+++ tests/day03/iframes-practice.test.js
@@ -7,16 +7,56 @@ test.describe("Test Group", () => {
     await page.goto("https://practice.cydeo.com/iframe");
   });
 
-  // Test 1
-  test("Locate the iframe by id", async ({ page }) => {});
 
-  // Test 2
-  test("Locate the iframe by name", async ({ page }) => {});
+  test("Locate the iframe by id", async ({ page }) => {
+    const myFrame = page.frameLocator("#mce_0_ifr");
 
-  // Test 3
-  test("Locate the iframe using CSS", async ({ page }) => {});
+    const elementInsideTheFrame = myFrame.locator("//body[@id='tinymce']");
+
+    //await elementInsideTheFrame.clear();
+
+   // await page.waitForTimeout(3000);
+    await elementInsideTheFrame.press("Control+A");
+   // await page.waitForTimeout(3000);
+    await elementInsideTheFrame.press("Backspace");
+   // await page.waitForTimeout(3000);
+
+    await elementInsideTheFrame.fill("Hello World!")
+
+    // pause the automation for 3 seconds
+    //await page.waitForTimeout(3000);
+
+    console.log(await elementInsideTheFrame.innerText());
+
+    await expect(elementInsideTheFrame).toHaveText("Hello World!");
+
+
+  });
+
+
+  test("Locate the iframe using CSS", async ({ page }) => {
+
+    const myFrame = page.frameLocator("iframe[id='mce_0_ifr'][title='Rich Text Area']");
+
+    const elementInsideTheFrame = myFrame.locator("//body[@id='tinymce']");
+
+    await expect(elementInsideTheFrame).toBeEnabled();
+
+  });
 
   // Test 4
-  test("Locate the iframe using Xpath", async ({ page }) => {});
+  test("Locate the iframe using Xpath", async ({ page }) => {
+    const myFrame = page.frameLocator("//iframe[@title='Rich Text Area']");
+
+    const elementInsideTheFrame = myFrame.locator("//body[@id='tinymce']");
+
+    await expect(elementInsideTheFrame).not.toBeDisabled();
+    await expect(elementInsideTheFrame).not.toBeHidden();
+  });
 
 });
+
+
+/*
+<iframe id="mce_0_ifr" frameborder="0" allowtransparency="true" title="Rich Text Area" class="tox-edit-area__iframe"></iframe>
+*/
