2ad9b881930db791140adf597633715bb5d8773b
Muhtar <Muhtar.Mahmut0519@gmail.com>
2024-07-27 11:07:58 -0400

    Playwright Day04

--- tests/day04/mouse-actions.test.js
+++ tests/day04/mouse-actions.test.js
@@ -11,17 +11,58 @@ test.describe("Test Group", async () => {
     await page.waitForTimeout(2000);
   });
 
-  test("", async ({ page }) => {
-
+  test("Left click test", async ({ page }) => {
+    await page.click("//a[@href='/inputs']");
+    expect(await page.title()).toBe("Inputs");
   });
 
-  test("A", async ({ page }) => {
-
+  test("Right click test", async ({ page }) => {
+    await page.waitForTimeout(2000);
+    await page.click("//a[@href='/inputs']", { button: "right" });
   });
 
-  test("B", async ({ page }) => {
+  test("Double click test", async ({ page }) => {
+    await page.dblclick("text='A/B Testing'");
+    expect(page.url()).toBe("https://practice.cydeo.com/abtest");
+  });
+
+  test("Double Click Practice2", async ({ page }) => {
+    await page.goto("https://the-internet.herokuapp.com/add_remove_elements/");
+    //button[contains(text(),'Add Element')]
+    await page.dblclick("//button[text()='Add Element']");
+    await expect(
+      page.locator("//button[contains(text(),'Delete')]")
+    ).toHaveCount(2);
+  });
+
+  test("Mouse hover test", async ({ page }) => {
+    await page.click("text='Hovers'");
+    await page.waitForTimeout(2000);
+    await page.hover("(//img[@src='/img/avatar-blank.jpg' and @alt='User Avatar'])[2]");
+    await expect(page.locator("text='name: user2'")).toBeVisible();
+    expect(await page.locator("text='name: user2'").innerText()).toBe("name: user2");
+  });
+
+
+  test("hover to multiple test", async ({ page }) => {
+    await page.click("text='Hovers'");
+
+    const elements = page.locator(
+      "//img[@src='/img/avatar-blank.jpg' and @alt='User Avatar']"
+    );
+
+    const invisibleElements = page.locator("//h5");
+
+    // create a for of loop that can iterate each elements of the elments
+    for (const each_element of await elements.all()) {
+      await page.waitForTimeout(2000);
+      await each_element.hover();
+    }
+  });
+
+  test("Drag and Drop test", async ({ page }) => {
+
 
   });
-  
 
 });
